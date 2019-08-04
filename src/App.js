df12113bf6129d64153e2277ee4e53407ed252c8
Marcel Bednarz <marcel.bednarz16@gmail.com>
2019-08-05 00:07:02 +0200

    add font awesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
+import "../src/App.scss";
 
-function App() {
-  return <div className="App">ll</div>;
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <section>
+        <div className="container">
+          <h2>Dollars</h2>
+          <form onSubmit="">
+            <input type="number" />
+            <select>
+              <option defaultValue="USD">USD</option>
+              <option value="PLN">PLN</option>
+              <option value="GBP">GBP</option>
+              <option value="EUR">EUR</option>
+              <option value="CHF">CHF</option>
+            </select>
+            <button>
+              <FontAwesomeIcon icon={faExchangeAlt} />
+            </button>
+          </form>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;

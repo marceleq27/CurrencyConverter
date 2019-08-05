2df9edc010d142f49d072d98c139df0ee686c063
Marcel Bednarz <marcel.bednarz16@gmail.com>
2019-08-05 18:03:45 +0200

    refactor scss

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
 import "../src/App.scss";
 
 class App extends Component {
-  state = {};
+  state = {
+    value: "420",
+    valueExchange: "40"
+  };
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+  handleClick = () => {
+    const svg = document.querySelector("svg");
+    svg.classList.toggle("active");
+  };
   render() {
+    const { value, valueExchange } = this.state;
     return (
       <section>
         <div className="container">
-          <h2>Dollars</h2>
+          <h2>United States Dollar</h2>
           <form onSubmit="">
-            <input type="number" />
+            <input type="number" value={value} onChange={this.handleChange} />
             <select>
               <option defaultValue="USD">USD</option>
               <option value="PLN">PLN</option>
@@ -19,10 +32,27 @@ class App extends Component {
               <option value="EUR">EUR</option>
               <option value="CHF">CHF</option>
             </select>
-            <button>
-              <FontAwesomeIcon icon={faExchangeAlt} />
-            </button>
           </form>
+        </div>
+        <button className="arrows" onClick={this.handleClick}>
+          <FontAwesomeIcon icon={faExchangeAlt} />
+        </button>
+        <div className="exchange">
+          <form onSubmit="">
+            <select>
+              <option defaultValue="USD">USD</option>
+              <option value="PLN">PLN</option>
+              <option value="GBP">GBP</option>
+              <option value="EUR">EUR</option>
+              <option value="CHF">CHF</option>
+            </select>
+            <input
+              type="number"
+              value={valueExchange}
+              onChange={this.handleChange}
+            />
+          </form>
+          <h2>Russian Ruble</h2>
         </div>
       </section>
     );

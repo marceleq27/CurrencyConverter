8a91f090e2cf3a63830e85016cf6730f82974d83
Marcel <marcel.bednarz16@gmail.com>
2020-06-21 14:33:56 +0200

    Updates

---
+++ precache-manifest.7b6e99552de51b690dc00cb82a1455d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98de3579e35ac3f2a5671b531fde3095",
+    "url": "/CurrencyConverter/index.html"
+  },
+  {
+    "revision": "f6b1f8dba915e6a5b637",
+    "url": "/CurrencyConverter/static/css/main.4947c43d.chunk.css"
+  },
+  {
+    "revision": "3c5732d8d721a83df983",
+    "url": "/CurrencyConverter/static/js/2.ca75880a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CurrencyConverter/static/js/2.ca75880a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b1f8dba915e6a5b637",
+    "url": "/CurrencyConverter/static/js/main.52179dcc.chunk.js"
+  },
+  {
+    "revision": "70f56cd6c768b23874da",
+    "url": "/CurrencyConverter/static/js/runtime-main.05b1b952.js"
+  }
+]);
